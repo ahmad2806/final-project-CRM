81e8c7419f973559ba5dd6e758560e78e613182c
ahmad2806 <ahmadhashem2806@gmail.com>
2018-11-10 23:18:46 +0200

    getting the events and splite them on the dashboard

--- front-end/src/app/dash-board/dash-board.component.ts
+++ front-end/src/app/dash-board/dash-board.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild, Input, ElementRef } from '@angular/core';
 import { EventService } from '../event/event.service';
 import { EventModel } from '../event/event.model';
 import { VolunteerModel } from "../volunteer/volunteer.model";
-
+import { UserService } from '../users/user.service';
+//TODO
 @Component({
   selector: 'app-dash-board',
   templateUrl: './dash-board.component.html',
@@ -15,7 +16,7 @@ export class DashBoardComponent implements OnInit {
   // public EventsInProgress = this.events.inProgressEvents.length;
   // public DeletedEvents = this.events.deletedEvents.length;
   // public CommingEvents = this.events.commingSoonEvents.length;
-  constructor(private events: EventService) {
+  constructor(private events: EventService, private userService: UserService) {
 
   }
   ngOnInit() {
@@ -26,24 +27,39 @@ export class DashBoardComponent implements OnInit {
     this.events.oldEvents = [];
     this.events.inProgressEvents = [];
     this.events.CSEDB = [];
-    for (let index = 0; index < this.events.commingSoonEvents.length; index++) {	
-      if (this.events.commingSoonEvents[index].date.getFullYear() > year) {// comming soon	
-        this.events.CSEDB.push(this.events.commingSoonEvents[index]);
+    let choose;
+    if (this.userService.activeUser.VolPer == true) {
+      choose = this.events.commingSoonEvents;
 
-   
+    } else {
+      choose = this.events.donorEvents;
+    }
+
+
+    for (let index = 0; index < choose.length; index++) {
+      if (typeof (choose[index].date) == "string") {
+        choose[index].date = new Date(choose[index].date);
+      }
+      if (choose[index].date.getFullYear() > year) {// comming soon
+        this.events.CSEDB.push(choose[index]);
+      } else if (choose[index].date.getFullYear() < year) { //  old events
+        this.events.oldEvents.push(choose[index]);
+      } else if (choose[index].date.getFullYear() == year) {
+        if (choose[index].date.getMonth() > month) { //comming soon
+          this.events.CSEDB.push(choose[index]);
         }
-        else if (this.events.commingSoonEvents[index].date.getMonth() < month) { // old
-          this.events.oldEvents.push(this.events.commingSoonEvents[index]);
-        } else if (this.events.commingSoonEvents[index].date.getMonth() == month) {
-          if (this.events.commingSoonEvents[index].date.getDay() > day) { // comming soon
-          } else if (this.events.commingSoonEvents[index].date.getDate() < day) { //old
-            this.events.oldEvents.push(this.events.commingSoonEvents[index]);
-          } else if (this.events.commingSoonEvents[index].date.getDate() == day) { // ingprogress today
-            this.events.inProgressEvents.push(this.events.commingSoonEvents[index]);
+        else if (choose[index].date.getMonth() < month) { // old
+          this.events.oldEvents.push(choose[index]);
+        } else if (choose[index].date.getMonth() == month) {
+          if (choose[index].date.getDay() > day) { // comming soon
+          } else if (choose[index].date.getDate() < day) { //old
+            this.events.oldEvents.push(choose[index]);
+          } else if (choose[index].date.getDate() == day) { // ingprogress today
+            this.events.inProgressEvents.push(choose[index]);
           }
         }
       }
-    
+    }
   }
 
   @Input() event: EventModel[] = [];
