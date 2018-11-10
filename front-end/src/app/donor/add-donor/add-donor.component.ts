1b9455a667ab1fa56a7e73c0e192cbe13114dc8c
ahmad2806 <ahmadhashem2806@gmail.com>
2018-11-11 00:22:34 +0200

    fixing donors type bugs

--- front-end/src/app/donor/add-donor/add-donor.component.ts
+++ front-end/src/app/donor/add-donor/add-donor.component.ts
@@ -5,7 +5,8 @@ import { DonorModel } from '../donor.model';
 import { Router } from '@angular/router';
 import { EventService } from '../../event/event.service';
 import { EventModel } from '../../event/event.model';
-
+import { Donate } from '../donate.model';
+//TODO
 @Component({
   selector: 'app-add-donor',
   templateUrl: './add-donor.component.html',
@@ -14,19 +15,20 @@ import { EventModel } from '../../event/event.model';
 export class AddDonorComponent implements OnInit {
 
   name = "";
-  lastname = "";
+  donateDate=new Date(2016,2,2);
   id = "";
   address = "";
   phone = "";
-  email = "";
+  email ;
   extraphone = "";
   birthday: Date;
   amount;
   discription = "";
+  date2: Date;
+  donate = [];
 
-
-  Foundation = true;
-  private = false;
+  Foundation = false;
+  private_donor = false;
   constructor(private donor: DonorService, private newEvent: EventService) { }
 
   ngOnInit() {
@@ -35,41 +37,50 @@ export class AddDonorComponent implements OnInit {
   radioChoice(choice: string) {
     if (choice == 'Foundation') {
       this.Foundation = true;
-      this.private = false;
+      this.private_donor = false;
     }
     if (choice == 'private') {
-      this.private = true;
+      this.private_donor = true;
       this.Foundation = false;
     }
   }
   reset() {
     this.name = "";
-    this.lastname = "";
+    this.donateDate=new Date();;
     this.id = "";
     this.address = "";
     this.phone = "";
-    this.email= "";
+    this.email = undefined;
     this.extraphone = "";
     this.birthday;
-    this.amount ;
+    this.amount = "" ;
     this.discription = "";
-    this.private = false;
-    this.Foundation = true;
+    this.private_donor = false;
+    this.Foundation = false;
   }
 
   save(exit) {
     let newDonor: DonorModel;
+    this.donateDate=new Date(this.donateDate);
+    
+    newDonor = new DonorModel(this.name,this.donateDate, this.id, this.birthday, this.address, this.phone, this.extraphone, this.email, "קרן", this.amount, [], this.discription, [new Donate(this.amount,this.date2)]);
+   
     if (this.Foundation == true) {
-      newDonor = new DonorModel(this.name,this.lastname, this.id, this.birthday, this.address, this.phone, this.extraphone, this.email, "קרן", this.amount, [],this.discription);	      
+      this.date2 = new Date();
+      this.date2.setDate(this.donateDate.getDate() + 365);
       this.donor.donor.push(newDonor);
     }
-    if (this.private == true) {
-      newDonor = new DonorModel(this.name,this.lastname, this.id, this.birthday, this.address, this.phone, this.extraphone, this.email, "פרטי", this.amount,[],this.discription);	
+    if (this.private_donor == true) {
+      newDonor = new DonorModel(this.name,this.donateDate, this.id, this.birthday, this.address, this.phone, this.extraphone, this.email, "פרטי", this.amount, [], this.discription, [new Donate(this.amount,this.date2)]);
+      this.date2 = new Date();
+      this.date2.setDate(this.donateDate.getDate() + 365);
       this.donor.donor.push(newDonor);
     }	
-    this.newEvent.add(new EventModel("לתרום שוב", "donor-Model", new Date(2018, 0, 8), "האם רוצה לתרום שוב",[] , [],[]));	
+    // this.newEvent.add(new EventModel("לתרום שוב", "donor-Model", new Date(2018, 0, 8), "האם רוצה לתרום שוב",[] , [],[]));	
     
     this.reset();
+    exit.click();
+
   }
 }
 
