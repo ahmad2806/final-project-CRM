ae936e252289b33401cdad64f429cdd1e24d89bb
ahmad2806 <ahmadhashem2806@gmail.com>
2018-12-01 21:40:05 +0200

    adding seeds data for volunteers

--- back-end/server/tests/seed/seed.js
+++ back-end/server/tests/seed/seed.js
@@ -1,7 +1,8 @@
 const { User } = require('./../../models/user');
- const jwt = require('jsonwebtoken');
+const { Volunteer } = require('./../../models/volunteer');
+const jwt = require('jsonwebtoken');
 const { ObjectID } = require('mongodb')
- const userOneId = new ObjectID();
+const userOneId = new ObjectID();
 const userTwoId = new ObjectID();
 const users = [{
   _id: userOneId,
@@ -10,28 +11,99 @@ const users = [{
   phone: '1234567890',
   username: 'ahmad',
   tokens: [{
-      access: 'auth',
-      token: jwt.sign({ _id: userOneId, access: 'auth' }, process.env.JWT_SECRET).toString()
-    }]
+    access: 'auth',
+    token: jwt.sign({ _id: userOneId, access: 'auth' }, process.env.JWT_SECRET).toString()
+  }]
 }, {
-    _id: userTwoId,
-    email: 'mohammad@exmple.com',
-    password: 'Two123456abc',
-    phone: '1234567890',
-    username: 'Hashem',
+  _id: userTwoId,
+  email: 'mohammad@exmple.com',
+  password: 'Two123456abc',
+  phone: '1234567890',
+  username: 'Hashem',
   tokens: [{
     access: 'auth',
     token: jwt.sign({ _id: userTwoId, access: 'auth' }, process.env.JWT_SECRET).toString()
   }]
 }];
- const wipeUsers = (done) => {
+const wipeUsers = (done) => {
   User.remove({}).then(() => {
     var userOne = new User(users[0]).save();
     var userTwo = new User(users[1]).save();
-     return Promise.all([userOne,userTwo])
+    return Promise.all([userOne, userTwo])
   }).then(() => done());
 };
- module.exports = {
-  users, 
-  wipeUsers
+
+/*********    volunteers    *****/
+const volunteerOneID = new ObjectID();
+const volunteerOneDaysID = new ObjectID();
+const volunteerTwoDaysID = new ObjectID();
+const volunteerTwoID = new ObjectID();
+const volunteers = [{
+  _id: volunteerOneID,
+  name: 'ahmad',
+  id: "205417744",
+  address: 'beit-hanina',
+  phone: '0524651749',
+  telePhone: '',
+  homePhone: '025859294',
+  email: 'AHMADLOXIZ@gmail.com',
+  volunteerType: 'cat',
+  job: 'software engineering',
+  avatar: '',
+  myEvents: [],
+  hasCar: true,
+  agreeToLeft: false,
+  freeDays: [
+    {
+      _id: volunteerOneDaysID,
+      saterday: false,
+      friday: false,
+      thursday: true,
+      wednesday: true,
+      tuesday: true,
+      monday: false,
+      sunday: true
+    }]
+}, {
+  _id: volunteerTwoID,
+  name: 'ahmad',
+  id: '123456789',
+  address: 'beit-hanina',
+  phone: '0524651749',
+  telePhone: '',
+  homePhone: '025859294',
+  email: 'AHMADLOXIZ@gmail.com',
+  volunteerType: 'dog',
+  job: 'software engineering',
+  avatar: '',
+  myEvents: [],
+  hasCar: true,
+  agreeToLeft: false,
+  freeDays: [
+    {
+      _id: volunteerTwoDaysID,
+      saterday: false,
+      friday: false,
+      thursday: true,
+      wednesday: true,
+      tuesday: true,
+      monday: false,
+      sunday: true
+    }
+  ]
+}];
+const wipeVolunteers = (done) => {
+  Volunteer.remove({}).then(() => {
+    var volunteerOne = new User(volunteers[0]).save();
+    var volunteerTwo = new User(volunteers[1]).save();
+     return Promise.all([volunteerOne,volunteerTwo])
+  }).then(() => done());
+};
+
+
+module.exports = {
+  users,
+  wipeUsers,
+  volunteers,
+  wipeVolunteers,
 } 
