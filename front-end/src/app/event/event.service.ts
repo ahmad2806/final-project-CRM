e5989d14fc1142e1f71799ad50bbe218166786c9
ahmad2806 <ahmadhashem2806@gmail.com>
2019-05-18 23:40:09 +0300

    making a shareable API for page changer

--- front-end/src/app/event/event.service.ts
+++ front-end/src/app/event/event.service.ts
@@ -27,29 +27,97 @@ export class EventService {
   date: Date;
 
   clicked: string = "";
+
+  m_all_items: any[] = []
+  elementsPerPage = 3
+  pdisabled = "previous disabled";
+  ndisabled = "next"
+  elementsToShow: any[];
+
+  CurrentPageNumber = 0;
+  previousPage = 0;
+  nextPage = 1;
+  Pages: number[] = [];
+
+  isEmpty = false;
+
+
   constructor() {
 
   }
-  public get CommingSoonEvents() {
-    return this.commingSoonEvents.slice;
-  }
-  public get DeletedEvents() {
-    return this.deletedEvents.slice;
-  }
-  public get OldEvents() {
-    return this.oldEvents.slice;
-  }
-  public get InProgressEvents() {
-    return this.inProgressEvents.slice;
-  }
-  public add(event: EventModel, type) {
-    if (type == "donor")
-      this.donorsEvents.push(event);
-    else
-      this.volunteersEvents.push(event);
-  }
+  
+
   public get Clicked() {
     return this.clicked;
+  }
+
+  resetPageParams() {
+    this.CurrentPageNumber = 0;
+    this.previousPage = 0;
+    this.nextPage = 1;
+    this.Pages = []
+    this.elementsToShow = []
+  }
+
+
+  public pageDivider(all_items) {
+    this.m_all_items = all_items;
+    this.resetPageParams();
+
+    let lenght = all_items.length;
+    let elementsOnPage = lenght / this.elementsPerPage;
+    let restOfTheElements = lenght % this.elementsPerPage;
+
+    if (all_items.length > 0) {
+
+      if (elementsOnPage < 1) {
+        if (restOfTheElements > 0) {
+          for (let i = 0; i < restOfTheElements; i++) {
+            this.elementsToShow.push(all_items[i]);
+          }
+        }
+      }
+      else {
+        for (let i = 0; i < this.elementsPerPage; i++) {
+          this.elementsToShow.push(all_items[i]);
+
+        }
+      }
+
+      for (let i = 0; i < elementsOnPage; i++) {
+        this.Pages[i] = i + 1;
+
+      }
+    } else {
+      this.isEmpty = true;
+    }
+  }
+
+  public ChangePage(pressedPage) {
+
+    if (pressedPage == this.Pages.length - 1) {
+      this.ndisabled = "next disabled";
+    }
+    else {
+      this.ndisabled = "next";
+      this.nextPage = pressedPage + 1;
+      this.CurrentPageNumber = pressedPage;
+    }
+    if (pressedPage == 0) {
+      this.pdisabled = "previous disabled";
+    } else {
+      this.pdisabled = "previous";
+      this.previousPage = pressedPage - 1;
+      this.CurrentPageNumber = pressedPage;
+
+    }
+    // TODO when searching, if number of result is more than this.elementsPerPage, problem my accure
+    // git the vlounteer for pressed page
+    this.elementsToShow = [];
+    for (let i = 0; i < this.elementsPerPage; i++) {
+      if (this.m_all_items[i + (pressedPage * this.elementsPerPage)] != undefined)
+        this.elementsToShow[i] = this.m_all_items[i + (pressedPage * this.elementsPerPage)];
+    }
   }
 
 }
